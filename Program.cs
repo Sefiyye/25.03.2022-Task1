b51a5e3c0d29eecc596209aeae3d22a42134dd8e
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-25 13:00:49 +0400

    Task

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Number is Odd?: " + IsOdd(num));
+            Console.WriteLine("Number is Even?: " + IsEven(num));
+        }
+        public static bool IsEven(int numbe)
+        {
+            bool check = false;
+            if (numbe % 2 == 0)
+            {
+                check = true;
+            }
+            return check;
+        }
+        public static bool IsOdd(int number)
+        {
+            bool TrueOrFalse = false;
+
+            if (number % 2 != 0)
+            {
+                TrueOrFalse = true;
+            }
+            return TrueOrFalse;
+        }
+    }
+}
